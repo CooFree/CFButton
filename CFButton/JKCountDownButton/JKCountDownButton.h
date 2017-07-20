2fd3199710db3fdceba2344d334fa2922d306889
Ken <coofree@foxmail.com>
2017-07-20 11:22:33 +0800

    2

--- CFButton/JKCountDownButton/JKCountDownButton.h
+++ CFButton/JKCountDownButton/JKCountDownButton.h
@@ -3,7 +3,7 @@
 //  JKCountDownButton
 //
 //  Created by Jakey on 15/3/8.
-//  Copyright (c) 2015年 www.skyfox.org. All rights reserved.
+//  Copyright © 2017年 CooFree. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- CFButton/ViewController.m
+++ CFButton/ViewController.m
@@ -7,19 +7,53 @@
 //
 
 #import "ViewController.h"
+#import "JKCountDownButton.h"
 
 @interface ViewController ()
-
+{
+    JKCountDownButton *_countDownCode;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self buildCountDown];
+
 }
+- (void)buildCountDown{
+    UILabel *lable = [[UILabel alloc]initWithFrame:CGRectMake(10, 200, 70, 32)];
+    lable.text = @"代码添加";
+    lable.textColor = [UIColor whiteColor];
+    [self.view addSubview:lable];
 
 
+    _countDownCode = [JKCountDownButton buttonWithType:UIButtonTypeCustom];
+    _countDownCode.frame = CGRectMake(81, 200, 108, 32);
+    [_countDownCode setTitle:@"开始" forState:UIControlStateNormal];
+    _countDownCode.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:_countDownCode];
+
+
+    [_countDownCode countDownButtonHandler:^(JKCountDownButton*sender, NSInteger tag) {
+        sender.enabled = NO;
+
+        [sender startCountDownWithSecond:10];
+
+        [sender countDownChanging:^NSString *(JKCountDownButton *countDownButton,NSUInteger second) {
+            NSString *title = [NSString stringWithFormat:@"剩余%zd秒",second];
+            return title;
+        }];
+        [sender countDownFinished:^NSString *(JKCountDownButton *countDownButton, NSUInteger second) {
+            countDownButton.enabled = YES;
+            return @"点击重新获取";
+
+        }];
+
+    }];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
